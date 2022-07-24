18297d034a208debdffab745be11f9a714a05414
Anthony Wildman <anthonywildman@hotmail.co.uk>
2022-07-24 21:26:45 +0100

    Set up files and added global styles

---
+++ utils/api.tsx
@@ -0,0 +1,10 @@
+const getCustomerInsights = async (customerId: string) => {
+	const insights = await fetch(
+		`https://api.jsonbin.io/v3/b/${customerId}?meta=false`
+	)
+		.then((result) => result.json())
+		.then((data) => data);
+	return insights;
+};
+
+export { getCustomerInsights };
